26b8604b0d4c759733f490799b125b3d7040b1cd
Bill Kendrick <nbs@sonic.net>
2021-04-19 23:50:22 -0700

    APOD server: ColorView: unique tmp filenames
    Use unique filenames when constructing the temp files for the
    ColorView (RGB) image files.  (Esp. important to avoid
    clobbering each other's files, when two users hit the script
    at the same time for two different modes, and/or APOD
    images from two different dates.)

--- apod/image_to_grrgb15.sh
+++ apod/image_to_grrgb15.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-rm img/tmp.pnm
-rm img/tmp.cv9
+uuid=$(uuidgen)
+fname="tmp_cv15_${uuid}"
 
-convert -size 160x192 xc:black - -resize 160x192 -gravity center -compose over -composite pnm:- > img/tmp.pnm
+rm img/${fname}.pnm
+rm img/${fname}.cv15
 
-convert img/tmp.pnm -channel R -separate -resize 80x192\! -set colorspace Gray -dither FloydSteinberg -depth 8 pgm:- > img/tmp_r.pgm
-convert img/tmp.pnm -channel G -separate -resize 80x192\! -set colorspace Gray -dither FloydSteinberg -depth 8 pgm:- > img/tmp_g.pgm
-convert img/tmp.pnm -channel B -separate -resize 80x192\! -set colorspace Gray -dither FloydSteinberg -depth 8 pgm:- > img/tmp_b.pgm
+convert -size 160x192 xc:black - -resize 160x192 -gravity center -compose over \
+  -composite pnm:- > img/${fname}.pnm
 
-cat img/tmp_r.pgm | ./pgm_to_gr15.php > img/tmp.cv15
-cat img/tmp_g.pgm | ./pgm_to_gr15.php >> img/tmp.cv15
-cat img/tmp_b.pgm | ./pgm_to_gr15.php >> img/tmp.cv15
+convert img/${fname}.pnm -channel R -separate -resize 80x192\! \
+  -set colorspace Gray -dither FloydSteinberg -depth 8 pgm:- \
+  > img/${fname}_r.pgm
+convert img/${fname}.pnm -channel G -separate -resize 80x192\! \
+  -set colorspace Gray -dither FloydSteinberg -depth 8 pgm:- \
+  > img/${fname}_g.pgm
+convert img/${fname}.pnm -channel B -separate -resize 80x192\! \
+  -set colorspace Gray -dither FloydSteinberg -depth 8 pgm:- \
+  > img/${fname}_b.pgm
 
-cat img/tmp.cv9
+cat img/${fname}_r.pgm | ./pgm_to_gr15.php > img/${fname}.cv15
+cat img/${fname}_g.pgm | ./pgm_to_gr15.php >> img/${fname}.cv15
+cat img/${fname}_b.pgm | ./pgm_to_gr15.php >> img/${fname}.cv15
 
+./interleave.php < img/${fname}.cv15
+
+# FIXME: Clean up
--- apod/image_to_grrgb9.sh
+++ apod/image_to_grrgb9.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-rm img/tmp.pnm
-rm img/tmp.cv9
+uuid=$(uuidgen)
+fname="tmp_cv9_${uuid}"
 
-convert -size 320x192 xc:black - -resize 320x192 -gravity center -compose over -composite pnm:- > img/tmp.pnm
+rm img/${fname}.pnm
+rm img/${fname}.cv9
 
-convert img/tmp.pnm -channel R -separate -resize 80x192\! -set colorspace Gray -dither FloydSteinberg -depth 8 pgm:- > img/tmp_r.pgm
-convert img/tmp.pnm -channel G -separate -resize 80x192\! -set colorspace Gray -dither FloydSteinberg -depth 8 pgm:- > img/tmp_g.pgm
-convert img/tmp.pnm -channel B -separate -resize 80x192\! -set colorspace Gray -dither FloydSteinberg -depth 8 pgm:- > img/tmp_b.pgm
+convert -size 320x192 xc:black - -resize 320x192 -gravity center -compose over \
+  -composite pnm:- > img/${fname}.pnm
 
-cat img/tmp_r.pgm | ./pgm_to_gr9.php > img/tmp.cv9
-cat img/tmp_g.pgm | ./pgm_to_gr9.php >> img/tmp.cv9
-cat img/tmp_b.pgm | ./pgm_to_gr9.php >> img/tmp.cv9
+convert img/${fname}.pnm -channel R -separate -resize 80x192\! \
+  -set colorspace Gray -dither FloydSteinberg -depth 8 pgm:- \
+  > img/${fname}_r.pgm
+convert img/${fname}.pnm -channel G -separate -resize 80x192\! \
+  -set colorspace Gray -dither FloydSteinberg -depth 8 pgm:- \
+  > img/${fname}_g.pgm
+convert img/${fname}.pnm -channel B -separate -resize 80x192\! \
+  -set colorspace Gray -dither FloydSteinberg -depth 8 pgm:- \
+  > img/${fname}_b.pgm
 
-./interleave.php < img/tmp.cv9
+cat img/${fname}_r.pgm | ./pgm_to_gr9.php > img/${fname}.cv9
+cat img/${fname}_g.pgm | ./pgm_to_gr9.php >> img/${fname}.cv9
+cat img/${fname}_b.pgm | ./pgm_to_gr9.php >> img/${fname}.cv9
 
+./interleave.php < img/${fname}.cv9
+
+# FIXME: Clean up
