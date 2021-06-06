180b6e916d7260e738882ccf807f6657a5c20d83
Bill Kendrick <nbs@sonic.net>
2021-06-06 00:57:22 -0700

    APOD Server: Slice/dice GR15DLI img for sane load
    Slice and dice the 192x44 bytes of image (40 bytes) & color (4 bytes)
    into 192x40 bytes of image, followed by 192x4 bytes of color,
    so it can be loaded in two chunks by the client.

--- apod/image_to_gr15dli.sh
+++ apod/image_to_gr15dli.sh
@@ -15,18 +15,31 @@ convert - -fuzz 1% -trim +repage pnm:- \
 | convert - -resize 160x192\! -depth 8 +dither -remap atari256.ppm pnm:- \
 > img/${fname}.pnm
 
-touch img/${fname}.gr15dli
+touch img/${fname}.gr15dli.img
+touch img/${fname}.gr15dli.pal
 
 for i in `seq 0 191`; do
+  rm img/${fname}.gr15dli.line
+
   convert img/${fname}.pnm \
     -crop 320x1+0+${i} pnm:- \
   | convert - -depth 8 +dither -colors 4 pnm:- \
   | convert - +dither -remap atari128.ppm -interpolate nearest pnm:- \
   | ./ppm_to_gr15.php 1 \
-  >> img/${fname}.gr15dli
+  > img/${fname}.gr15dli.line
+
+  dd iflag=count_bytes,skip_bytes bs=1 count=40 \
+    < img/${fname}.gr15dli.line >> img/${fname}.gr15dli.img
+  dd iflag=count_bytes,skip_bytes skip=40 bs=1 count=4 \
+    < img/${fname}.gr15dli.line >> img/${fname}.gr15dli.pal
+
 done
 
 rm img/${fname}.pnm
-cat img/${fname}.gr15dli
-rm img/${fname}.gr15dli
+
+cat img/${fname}.gr15dli.img
+cat img/${fname}.gr15dli.pal
+
+rm img/${fname}.gr15dli.img
+rm img/${fname}.gr15dli.pal
 
