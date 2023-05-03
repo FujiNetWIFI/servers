530daae1b9f311f92f0137e3412d5f6a4a271c0a
Roger Sen <roger.sen@gmail.com>
2023-05-03 22:07:14 +0200

    Added main set of test scenarios

--- lobby/TODO.md
+++ lobby/TODO.md
@@ -1,6 +1,7 @@
 # Additional improvements to the lobby server
 
-* Create test scenarios.
+* Create test scenarios (ongoing)
+* In test scenarions set client url to TNFS://
 * Deploy CI/CD.
 * Develop a reaper process to remove extremely old and offline servers from the database.
 * Provide persistance to the servers stored dumping GAMESRV to a json file every X seconds.
--- lobby/main_test.go
+++ lobby/main_test.go
@@ -13,6 +13,262 @@ import (
 
 var ROUTER = setupRouter()
 
+var GameServersIn = []string{
+	`{
+        "game": "Super Chess",
+        "gametype": 1,
+        "server": "chess.rogersm.net",
+        "serverurl": "http://chess.rogersm.net/server",
+        "region": "eu",
+        "status": "online",
+        "maxplayers": 2,
+        "curplayers": 1,
+        "clients": [
+            {"platform":"atari", "url":"http://chess.rogersm.net/atarichess.xex" },
+            {"platform": "spectrum", "url":"http://chess.rogersm.net/speccychess.xex"},
+            {"platform": "c64", "url":"http://chess.rogersm.net/c64chess.xex"}
+
+        ]
+    }`,
+	`{
+        "game": "Battleship",
+        "gametype": 1,
+        "region": "au",
+        "server": "8bitBattleship.com",
+        "serverurl": "https://8bitBattleship.com/battlebots",
+        "status": "online",
+        "maxplayers": 2,
+        "curplayers": 1,
+        "clients": [
+            {"platform":"atari", "url":"https://8bitBattleship.com/atariship.xex" },
+            {"platform": "spectrum", "url":"https://8bitBattleship.com/specship.xex"},
+            {"platform": "c64", "url":"https://8bitBattleship.com/c64ship.xex"},
+            {"platform": "amiga", "url":"https://8bitBattleship.com/amigaship.xex"}
+        ]
+	}`,
+	`{
+        "game": "5 CARD STUD",
+        "gametype": 1,
+        "region": "us",
+        "server": "erichomeserver.com",
+        "serverurl": "tcp://thomcorner.com/pokerbots",
+        "status": "online",
+        "maxplayers": 8,
+        "curplayers": 1,
+        "clients": [
+            {"platform":"atari", "url":"tcp://thomcorner.com/clientus/ataripoker.xex" },
+            {"platform": "spectrum", "url":"tcp://thomcorner.com/clientus/specpoker.xex"},
+            {"platform": "c64", "url":"tcp://thomcorner.com/clientus/c64poker.xex"},
+            {"platform": "lynx", "url":"tcp://thomcorner.com/clientus/lynxpoker.xex"}
+        ]
+	}`,
+	`{
+        "game": "Battleship",
+        "gametype": 1,
+        "region": "apac",
+        "server": "8bitBattleship.com",
+        "serverurl": "https://8bitBattleship.com/battlehuman",
+        "status": "online",
+        "maxplayers": 2,
+        "curplayers": 0,
+        "clients": [
+            {"platform":"atari", "url":"https://8bitBattleship.com/atariship.xex" },
+            {"platform": "spectrum", "url":"https://8bitBattleship.com/specship.xex"},
+            {"platform": "c64", "url":"https://8bitBattleship.com/c64ship.xex"},
+            {"platform": "amiga", "url":"https://8bitBattleship.com/amigaship.xex"},
+            {"platform": "vic20", "url":"https://8bitBattleship.com/vic20ship.xex"}
+
+        ]
+    }`,
+	`{
+        "game": "5 CARD STUD",
+        "gametype": 1,
+        "region": "all",
+        "server": "erichomeserver.com",
+        "serverurl": "tcp://thomcorner.com/server5",
+        "status": "offline",
+        "maxplayers": 3,
+        "curplayers": 0,
+        "clients": [
+            {"platform":"atari", "url":"tcp://thomcorner.com/ataripoker.xex" },
+            {"platform": "spectrum", "url":"tcp://thomcorner.com/specpoker.xex"},
+            {"platform": "c64", "url":"tcp://thomcorner.com/c64poker.xex"},
+            {"platform": "amiga", "url":"tcp://thomcorner.com/amigapoker.xex"}
+        ]
+    }`,
+	`{
+        "game": "5 CARD STUD",
+        "gametype": 1,
+        "region": "vatican",
+        "server": "thomcorner.com",
+        "serverurl": "tcp://thomcorner.com/pokerhuman",
+        "status": "online",
+        "maxplayers": 8,
+        "curplayers": 4,
+        "clients": [
+            {"platform":"atari", "url":"tcp://thomcorner.com/clt/ataripoker.xex" }  
+        ]
+    }`}
+
+var GameServersOut = `"[
+    {
+        "game": "5 CARD STUD",
+        "gametype": 1,
+        "server": "thomcorner.com",
+        "region": "vatican",
+        "serverurl": "tcp://thomcorner.com/pokerhuman",
+        "status": "online",
+        "maxplayers": 8,
+        "curplayers": 4,
+        "clients": [
+            {
+                "platform": "atari",
+                "url": "tcp://thomcorner.com/clt/ataripoker.xex"
+            }
+        ]
+    },
+    {
+        "game": "Battleship",
+        "gametype": 1,
+        "server": "8bitBattleship.com",
+        "region": "apac",
+        "serverurl": "https://8bitBattleship.com/battlehuman",
+        "status": "online",
+        "maxplayers": 2,
+        "curplayers": 0,
+        "clients": [
+            {
+                "platform": "atari",
+                "url": "https://8bitBattleship.com/atariship.xex"
+            },
+            {
+                "platform": "spectrum",
+                "url": "https://8bitBattleship.com/specship.xex"
+            },
+            {
+                "platform": "c64",
+                "url": "https://8bitBattleship.com/c64ship.xex"
+            },
+            {
+                "platform": "amiga",
+                "url": "https://8bitBattleship.com/amigaship.xex"
+            },
+            {
+                "platform": "vic20",
+                "url": "https://8bitBattleship.com/vic20ship.xex"
+            }
+        ]
+    },
+    {
+        "game": "5 CARD STUD",
+        "gametype": 1,
+        "server": "erichomeserver.com",
+        "region": "us",
+        "serverurl": "tcp://thomcorner.com/pokerbots",
+        "status": "online",
+        "maxplayers": 8,
+        "curplayers": 1,
+        "clients": [
+            {
+                "platform": "atari",
+                "url": "tcp://thomcorner.com/clientus/ataripoker.xex"
+            },
+            {
+                "platform": "spectrum",
+                "url": "tcp://thomcorner.com/clientus/specpoker.xex"
+            },
+            {
+                "platform": "c64",
+                "url": "tcp://thomcorner.com/clientus/c64poker.xex"
+            },
+            {
+                "platform": "lynx",
+                "url": "tcp://thomcorner.com/clientus/lynxpoker.xex"
+            }
+        ]
+    },
+    {
+        "game": "Battleship",
+        "gametype": 1,
+        "server": "8bitBattleship.com",
+        "region": "au",
+        "serverurl": "https://8bitBattleship.com/battlebots",
+        "status": "online",
+        "maxplayers": 2,
+        "curplayers": 1,
+        "clients": [
+            {
+                "platform": "atari",
+                "url": "https://8bitBattleship.com/atariship.xex"
+            },
+            {
+                "platform": "spectrum",
+                "url": "https://8bitBattleship.com/specship.xex"
+            },
+            {
+                "platform": "c64",
+                "url": "https://8bitBattleship.com/c64ship.xex"
+            },
+            {
+                "platform": "amiga",
+                "url": "https://8bitBattleship.com/amigaship.xex"
+            }
+        ]
+    },
+    {
+        "game": "Super Chess",
+        "gametype": 1,
+        "server": "chess.rogersm.net",
+        "region": "eu",
+        "serverurl": "http://chess.rogersm.net/server",
+        "status": "online",
+        "maxplayers": 2,
+        "curplayers": 1,
+        "clients": [
+            {
+                "platform": "atari",
+                "url": "http://chess.rogersm.net/atarichess.xex"
+            },
+            {
+                "platform": "spectrum",
+                "url": "http://chess.rogersm.net/speccychess.xex"
+            },
+            {
+                "platform": "c64",
+                "url": "http://chess.rogersm.net/c64chess.xex"
+            }
+        ]
+    },
+    {
+        "game": "5 CARD STUD",
+        "gametype": 1,
+        "server": "erichomeserver.com",
+        "region": "all",
+        "serverurl": "tcp://thomcorner.com/server5",
+        "status": "offline",
+        "maxplayers": 3,
+        "curplayers": 0,
+        "clients": [
+            {
+                "platform": "atari",
+                "url": "tcp://thomcorner.com/ataripoker.xex"
+            },
+            {
+                "platform": "spectrum",
+                "url": "tcp://thomcorner.com/specpoker.xex"
+            },
+            {
+                "platform": "c64",
+                "url": "tcp://thomcorner.com/c64poker.xex"
+            },
+            {
+                "platform": "amiga",
+                "url": "tcp://thomcorner.com/amigapoker.xex"
+            }
+        ]
+    }
+]`
+
 func setupRouter() *gin.Engine {
 
 	router := gin.Default()
@@ -24,23 +280,13 @@ func setupRouter() *gin.Engine {
 	return router
 }
 
-func REMOVEassertHTTPAnswerText(w *httptest.ResponseRecorder, HTTPCode int, HTTPBody string) (err []error) {
-
-	if w.Code != HTTPCode {
-		err = append(err, fmt.Errorf("Expecing HTTP %d, received HTTP %d", HTTPCode, w.Code))
-	}
-
-	if w.Body.String() != HTTPBody {
-		err = append(err, fmt.Errorf("Expecting body: %s, received %s", w.Body.String(), HTTPBody))
-	}
-
-	return err
-
-}
-
 func assertHTTPAnswerJSON(w *httptest.ResponseRecorder, HTTPCode int, HTTPBody string) (err []error) {
 	if w.Code != HTTPCode {
 		err = append(err, fmt.Errorf("Expecing HTTP %d, received HTTP %d", HTTPCode, w.Code))
+	}
+
+	if w.Body.String() == HTTPBody {
+		return err
 	}
 
 	opts := jsondiff.DefaultJSONOptions()
@@ -59,6 +305,8 @@ func assertHTTPAnswerJSON(w *httptest.ResponseRecorder, HTTPCode int, HTTPBody s
 func TestEmptyViewFull(t *testing.T) {
 
 	w := httptest.NewRecorder()
+	w.Header().Add("Content-Type", "application/json")
+
 	req, _ := http.NewRequest("GET", "/viewFull", nil)
 	ROUTER.ServeHTTP(w, req)
 
@@ -72,6 +320,8 @@ func TestEmptyViewFull(t *testing.T) {
 func TestEmptyView(t *testing.T) {
 
 	w := httptest.NewRecorder()
+	w.Header().Add("Content-Type", "application/json")
+
 	req, _ := http.NewRequest("GET", "/view", nil)
 	ROUTER.ServeHTTP(w, req)
 
@@ -84,13 +334,14 @@ func TestEmptyView(t *testing.T) {
 
 func TestInsertServer1(t *testing.T) {
 	w := httptest.NewRecorder()
+	w.Header().Add("Content-Type", "application/json")
+
 	req, _ := http.NewRequest("POST", "/server", bytes.NewBuffer([]byte(`{
         "game": "Super Chess",
         "gametype": 1,
         "server": "chess.rogersm.net",
-        "serverURL": "http://chess.rogersm.net/server",
+        "serverurl": "http://chess.rogersm.net/server",
         "region": "eu",
-        "instance": "Table A",
         "status": "online",
         "maxplayers": 2,
         "curplayers": 1,
@@ -109,55 +360,27 @@ func TestInsertServer1(t *testing.T) {
 
 }
 
-func TestViewInsertAndRetrieveServer1(t *testing.T) {
+func TestViewFullInsertAndRetrieveServerN(t *testing.T) {
+
+	for _, ServerJson := range GameServersIn {
+
+		w := httptest.NewRecorder()
+		w.Header().Add("Content-Type", "application/json")
+		req, _ := http.NewRequest("POST", "/server", bytes.NewBuffer([]byte(ServerJson)))
+		ROUTER.ServeHTTP(w, req)
+
+		if errors := assertHTTPAnswerJSON(w, 201, `{"message":"Server correctly updated","success":true}`); errors != nil {
+			for _, err := range errors {
+				t.Errorf("%s %s %s", req.Method, req.URL.Path, err)
+			}
+		}
+	}
+
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("POST", "/server", bytes.NewBuffer([]byte(`{
-        "game": "Super Chess",
-        "gametype": 1,
-        "server": "chess.rogersm.net",
-        "serverurl": "http://chess.rogersm.net/server",
-        "region": "eu",
-        "status": "online",
-        "maxplayers": 2,
-        "curplayers": 1,
-        "clients": [
-            {"platform":"atari", "url":"http://chess.rogersm.net/atarichess.xex" },
-            {"platform": "spectrum", "url":"http://chess.rogersm.net/speccychess.xex"}
-        ]
-    }`)))
+	req, _ := http.NewRequest("GET", "/viewFull", nil)
 	ROUTER.ServeHTTP(w, req)
 
-	if errors := assertHTTPAnswerJSON(w, 201, `{"message":"Server correctly updated","success":true}`); errors != nil {
-		for _, err := range errors {
-			t.Errorf("%s %s %s", req.Method, req.URL.Path, err)
-		}
-	}
-
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest("GET", "/view", nil)
-	ROUTER.ServeHTTP(w, req)
-
-	if errors := assertHTTPAnswerJSON(w, 400, `{"message":"You need to submit a platform","success":false}`); errors != nil {
-		for _, err := range errors {
-			t.Errorf("%s %s %s", req.Method, req.URL.Path, err)
-		}
-	}
-
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest("GET", "/view?platform=atari", nil)
-	ROUTER.ServeHTTP(w, req)
-
-	if errors := assertHTTPAnswerJSON(w, 200, `[{"g":"Super Chess","t":1,"u":"http://chess.rogersm.net/server","c":"http://chess.rogersm.net/atarichess.xex","s":"chess.rogersm.net","r":"eu","o":1,"m":2,"p":1}]`); errors != nil {
-		for _, err := range errors {
-			t.Errorf("%s %s %s", req.Method, req.URL.Path, err)
-		}
-	}
-
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest("GET", "/view?platform=spectrum", nil)
-	ROUTER.ServeHTTP(w, req)
-
-	if errors := assertHTTPAnswerJSON(w, 200, `[{"g":"Super Chess","t":1,"u":"http://chess.rogersm.net/server","c":"http://chess.rogersm.net/speccychess.xex","s":"chess.rogersm.net","r":"eu","o":1,"m":2,"p":1}]`); errors != nil {
+	if errors := assertHTTPAnswerJSON(w, 200, GameServersOut); errors != nil {
 		for _, err := range errors {
 			t.Errorf("%s %s %s", req.Method, req.URL.Path, err)
 		}
@@ -165,48 +388,27 @@ func TestViewInsertAndRetrieveServer1(t *testing.T) {
 
 }
 
-func TestViewFullInsertAndRetrieveServer1(t *testing.T) {
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("POST", "/server", bytes.NewBuffer([]byte(`{
-        "game": "Super Chess",
-        "gametype": 1,
-        "server": "chess.rogersm.net",
-        "serverurl": "http://chess.rogersm.net/server",
-        "region": "eu",
-        "status": "online",
-        "maxplayers": 2,
-        "curplayers": 1,
-        "clients": [
-            {"platform":"atari", "url":"http://chess.rogersm.net/atarichess.xex" },
-            {"platform": "spectrum", "url":"http://chess.rogersm.net/speccychess.xex"}
-        ]
-    }`)))
-	ROUTER.ServeHTTP(w, req)
+func TODOTestViewInsertAndRetrieveServerN(t *testing.T) {
 
-	if errors := assertHTTPAnswerJSON(w, 201, `{"message":"Server correctly updated","success":true}`); errors != nil {
-		for _, err := range errors {
-			t.Errorf("%s %s %s", req.Method, req.URL.Path, err)
+	for _, ServerJson := range GameServersIn {
+
+		w := httptest.NewRecorder()
+		w.Header().Add("Content-Type", "application/json")
+		req, _ := http.NewRequest("POST", "/server", bytes.NewBuffer([]byte(ServerJson)))
+		ROUTER.ServeHTTP(w, req)
+
+		if errors := assertHTTPAnswerJSON(w, 201, `{"message":"Server correctly updated","success":true}`); errors != nil {
+			for _, err := range errors {
+				t.Errorf("%s %s %s", req.Method, req.URL.Path, err)
+			}
 		}
 	}
 
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest("GET", "/viewFull", nil)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/view", nil)
 	ROUTER.ServeHTTP(w, req)
 
-	if errors := assertHTTPAnswerJSON(w, 200, `[{
-        "game": "Super Chess",
-        "gametype": 1,
-        "server": "chess.rogersm.net",
-        "serverurl": "http://chess.rogersm.net/server",
-        "region": "eu",
-        "status": "online",
-        "maxplayers": 2,
-        "curplayers": 1,
-        "clients": [
-            {"platform":"atari", "url":"http://chess.rogersm.net/atarichess.xex" },
-            {"platform": "spectrum", "url":"http://chess.rogersm.net/speccychess.xex"}
-        ]
-    }]`); errors != nil {
+	if errors := assertHTTPAnswerJSON(w, 200, GameServersOut); errors != nil {
 		for _, err := range errors {
 			t.Errorf("%s %s %s", req.Method, req.URL.Path, err)
 		}
