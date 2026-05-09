4a6b093d87b723885bf9f2ef223dfaf76a9ba656
Eric Carr <eric.carr@gmail.com>
2026-05-09 15:22:13 -0500

    Fujitzee - add msdos client to lobby

--- fujinet-game-system/fujitzee/server/lobbyClient.go
+++ fujinet-game-system/fujitzee/server/lobbyClient.go
@@ -28,6 +28,7 @@ var DefaultGameServerDetails = GameServer{
 		{Platform: "atari", Url: "tnfs://ec.tnfs.io/atari/fujitzee.xex"},
 		{Platform: "apple2", Url: "tnfs://ec.tnfs.io/apple2/fujitzee.po"},
 		{Platform: "coco", Url: "tnfs://ec.tnfs.io/coco/fujitzee.dsk"},
+		{Platform: "msdos", Url: "tnfs://ec.tnfs.io/msdos/fujitzee.img"},
 	},
 }
 
